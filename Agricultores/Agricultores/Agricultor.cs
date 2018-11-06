a3e068bec874b212a9c3c397d7a46676cb764867
RaymiIrahola <raymiirahola@gmail.com>
2018-11-06 11:24:58 -0300

    Clases agregadas

---
+++ Agricultores/Agricultores/Agricultor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agricultores
+{
+    public class Agricultor: Persona
+    {
+        public int cantParcelas { get; set; }
+        public string nombre { get; set; }
+        public Agricultor(int unCantParcelas, string unNombre): base (unNombre)
+        {
+            cantParcelas = unCantParcelas;
+            nombre = unNombre;
+        }
+    }
+}
---
+++ Agricultores/Agricultores/Comprador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agricultores
+{
+    public class Comprador: Persona
+    {
+        public string nombre { get; set; }
+        public int porcAjuste { get; set; }
+        public Comprador(int unPorcAjuste, string unNombre): base (unNombre)
+        {
+            nombre = unNombre;
+            porcAjuste = unPorcAjuste;
+        }
+    }
+}
